52b0b14f6c7ccb84b479f57b4bc595108470b84b
fu2020rao <1772885162@qq.com>
2020-06-28 11:49:30 +0800

    第三次提交

--- src/java123/Test.java
+++ src/java123/Test.java
@@ -1,10 +1,23 @@
 package java123;
 
-public class Test {
+public class Test extends Thread {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+@Override
+	public void run() {
+		//TODO Auto-generated method stub
+		// super.run();
+		for (int i=0;i<2000;i++) {
+			System.out.println(i+ "+++多线程+++");
+		}
 	}
 
+	public static void main(String[] args) {
+		Test thread =new Test();
+		thread.start();
+		// TODO Auto-generated method stub
+for (int i=0; i<=2000; i++) {
+	System.out.println(i + "---主线方程---");
+}
+
+}
 }
